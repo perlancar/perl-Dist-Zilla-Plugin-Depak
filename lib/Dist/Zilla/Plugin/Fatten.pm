a6cdfc60fa6960d3b09ef567bbf64e6d682c55b8
perlancar (@pc-home) <perlancar@gmail.com>
2015-05-16 18:42:57 +0700

    Replace # FATTENED_MODULES and # FATTENED_DISTS with 'our @FATTENED_MODULES' and 'our @FATTENED_DISTS' to help tools

--- lib/Dist/Zilla/Plugin/Fatten.pm
+++ lib/Dist/Zilla/Plugin/Fatten.pm
@@ -7,6 +7,8 @@ use 5.010001;
 use strict;
 use warnings;
 
+use App::lcpan::Call qw(call_lcpan_script);
+use Data::Dmp;
 use File::Temp qw(tempfile);
 use File::Which;
 use IPC::System::Options qw(system);
@@ -14,10 +16,10 @@ use List::Util qw(first);
 
 use Moose;
 with (
-    'Dist::Zilla::Role::FileMunger',
     'Dist::Zilla::Role::FileFinderUser' => {
-        default_finders => [':ExecFiles'],
+        default_finders => [':ExecFiles', ':InstallModules'],
     },
+    'Dist::Zilla::Role::FileMunger',
 );
 
 # TODO: fatten_path?
@@ -33,9 +35,8 @@ sub munge_files {
 
     my $self = shift;
 
-    my @scripts0 = @{ $self->found_files };
+    my @scripts0 = grep { $_->name =~ m!^(bin|scripts?)! } @{ $self->found_files };
     my @scripts;
-
     if ($self->include_script) {
         for my $item (@{ $self->include_script }) {
             my $file = first { $item eq $_->name } @scripts0;
@@ -46,17 +47,18 @@ sub munge_files {
     } else {
         @scripts = @scripts0;
     }
-
     if ($self->exclude_script) {
         for my $item (@{ $self->exclude_script }) {
             @scripts = grep { $_->name ne $item } @scripts;
         }
     }
+    $self->munge_script($_) for @scripts;
 
-    $self->munge_file($_) for @scripts;
+    my @modules  = grep { $_->name =~ m!^(lib)! } @{ $self->found_files };
+    $self->munge_module($_) for @modules;
 }
 
-sub munge_file {
+sub munge_script {
     my ($self, $file) = @_;
 
     $self->log_fatal(["Can't find fatten in PATH"]) unless which("fatten");
@@ -90,7 +92,49 @@ sub munge_file {
         local $/;
         ~~<$fh>;
     };
+
+    while ($content =~ /^\$fatpacked\{"(.+?)\.pm"\}/mg) {
+        my $mod = $1;
+        $mod =~ s!/!::!g;
+        $self->{_mods}{$mod} = 0;
+    }
+
     $file->content($content);
+}
+
+sub munge_module {
+    my ($self, $file) = @_;
+
+    my $munged;
+    my $content = $file->content;
+    if ($content =~ /^#\s*FATTENED_MODULES\s*$/m) {
+        $munged++;
+        $self->{_mods} //= {};
+        $content =~ s/(^#\s*FATTENED_MODULES\s*$)/
+            "our \@FATTENED_MODULES = \@{" . dmp(sort keys %{$self->{_mods}}) . "}; $1"/em;
+    }
+
+    if ($content =~ /^#\s*FATTENED_DISTS\s*$/m) {
+        $munged++;
+        unless ($self->{_dists}) {
+            if (!keys %{ $self->{_mods} }) {
+                $self->{_dists} = {};
+            } else {
+                my $res = call_lcpan_script(
+                    argv => ["mod2dist", keys %{$self->{_mods}}],
+                );
+                for (values %$res) {
+                    $self->{_dists}{$_} = 0;
+                }
+            }
+        }
+        $content =~ s/(^#\s*FATTENED_DISTS\s*$)/
+            "our \@FATTENED_DISTS = \@{" . dmp(sort keys %{$self->{_dists}}) . "}; $1"/em;
+    }
+
+    if ($munged) {
+        $file->content($content);
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
@@ -101,7 +145,7 @@ __PACKAGE__->meta->make_immutable;
 
 =head1 SYNOPSIS
 
-In C<dist.ini> in dist top-level directory:
+In C<dist.ini>:
 
  [Fatten]
  ;;; the default is to include all scripts, but use below to include only some
@@ -113,6 +157,17 @@ In C<fatten.conf> in dist top-level directory, put your L<fatten> configuration.
 
 During build, your scripts will be replaced with the fatpacked version.
 
+Also, you should also have a module named C<Something::Fattened> (i.e. whose
+name ends in C<::Fattened>), which contains:
+
+ # FATTENED_MODULES
+ # FATTENED_DISTS
+
+During build, these will be replaced with:
+
+ our @FATTENED_MODULES = (...); # FATTENED_MODULES
+ our @FATTENED_DISTS = (...); # FATTENED_DISTS
+
 
 =head1 DESCRIPTION
 
@@ -121,6 +176,15 @@ will be done using L<fatten>.
 
 If C<fatten.conf> exists in your dist's top-level directory, it will be used as
 the fatten configuration.
+
+In addition to replacing scripts with the fatpacked version, it will also search
+for directives C<# FATTENED_MODULES> and C<# FATTENED_DISTS> in module files and
+replace them with C<@FATTENED_MODULES> and C<@FATTENED_DISTS>. The
+C<@FATTENED_MODULES> array lists all the modules that are included in the one of
+the scripts. This can be useful for tools that might need it. C<@FATTENED_DISTS>
+array lists all the dists that are included in one of the scripts. This also can
+be useful for tools that might need it, like
+L<Dist::Zilla::Plugin::PERLANCAR::CheckDepDists>.
 
 
 =head1 CONFIGURATION
