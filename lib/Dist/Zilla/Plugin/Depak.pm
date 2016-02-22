97b2e46aaaf5f9ce20c0ac410e6399eb1ec1f7c1
perlancar (@netbook-zenbook) <perlancar@gmail.com>
2016-02-22 19:23:35 +0700

    [Bugfix] Handle case when 'lcpan mod2dist' returns only a single result (str instead of hash)

--- lib/Dist/Zilla/Plugin/Depak.pm
+++ lib/Dist/Zilla/Plugin/Depak.pm
@@ -197,8 +197,13 @@ sub munge_module {
                 );
                 $self->log_fatal(["Can't lcpan mod2dist: %s - %res", $res->[0], $res->[1]])
                     unless $res->[0] == 200;
-                for (values %{$res->[2]}) {
-                    $self->{_dists}{$_} = 0;
+                if (ref $res->[2]) {
+                    for (values %{$res->[2]}) {
+                        $self->{_dists}{$_} = 0;
+                    }
+                } else {
+                    # single result
+                    $self->{_dists}{$res->[2]} = 0;
                 }
             }
         }
